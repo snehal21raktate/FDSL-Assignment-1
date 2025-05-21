a121068b0c23077f47ff92066e716e908cbbd057
snehal21raktate <raktatesnehal2@gmail.com>
2025-05-21 12:49:26 +0530

    Add files via upload

---
+++ main(1).cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int linearSearch(int arr[], int size, int target) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == target) {
+            return i; 
+        }
+    }
+    return -1; 
+}
+
+int main() {
+    int numbers[] = {5, 3, 8, 4, 2};
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+    int target = 4;
+
+    int result = linearSearch(numbers, size, target);
+
+    if (result != -1) {
+        printf("Element found at index %d\n", result);
+    } else {
+        printf("Element not found\n");
+    }
+
+    return 0;
+}
